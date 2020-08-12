00cf78f906d0dc1079859503fea00bb3288ec996
unknown <thisisnew@naver.com>
2020-08-12 22:33:54 +0900

    Updates

---
+++ precache-manifest.991e44b799b0008c6512b8c10dd25051.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "510206e5b8cb3478f6ea84f637f24b06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0cf92133b1b5994e44ea",
+    "url": "/static/css/main.5721af45.chunk.css"
+  },
+  {
+    "revision": "22cb90122fd5507e6a66",
+    "url": "/static/js/2.ff56ef60.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ff56ef60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cf92133b1b5994e44ea",
+    "url": "/static/js/main.7e661ee6.chunk.js"
+  },
+  {
+    "revision": "bd7f4f55b55e38cea2f1",
+    "url": "/static/js/runtime-main.58002af7.js"
+  }
+]);
